a0fe703ad40b6aa47531b9615a2407c4bf5c3005
ash162011 <83339498+ash162011@users.noreply.github.com>
2021-12-17 18:15:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+rightWristX = 0;
+rightWristY = 0;
+scoreRightWrist = 0;
+
+function setup(){
+canvas =  createCanvas(600,500);
+canvas.parent('canvas');
+
+video = createCapture(VIDEO);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results){
+	if(results.length > 0){
+		rightWristX = results[0].pose.nose.x;
+	  rightWristY = results[0].pose.nose.y;
+	  console.log(results);
+
+	}
+}
+
+function draw(){
+  image(video, 0, 0, 700, 600);
+
+  if(scoreRightWrist > 0.2){
+    fill("#ffdde4");
+    stroke("#ffdde4");
+    circle(rightWristX, rightWristY, 30)
+  }
+}
